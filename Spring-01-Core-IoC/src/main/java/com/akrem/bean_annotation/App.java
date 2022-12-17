cb0dc2547f44a44c3e0dccd30c2154cc83475f69
akrem-cpu <akremabdu71@gmail.com>
2022-12-17 15:44:54 -0600

    Initial commit

---
+++ Spring-01-Core-IoC/src/main/java/com/akrem/bean_annotation/App.java
@@ -0,0 +1,16 @@
+package com.akrem.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+       ApplicationContext container = new AnnotationConfigApplicationContext();
+
+
+
+
+
+
+    }
+}
---
+++ Spring-01-Core-IoC/src/main/java/com/akrem/bean_annotation/PartTimeMentor.java
@@ -0,0 +1,7 @@
+package com.akrem.bean_annotation;
+
+public class PartTimeMentor {
+    public void createAccount(){
+        System.out.println("Part Time Mentor is Created");
+    }
+}
