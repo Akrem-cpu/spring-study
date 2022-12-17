50dc868a566f06c86af79051408fd6e3a672ec1c
akrem-cpu <akremabdu71@gmail.com>
2022-12-17 15:54:28 -0600

    all about IoC

--- Spring-01-Core-IoC/src/main/java/com/akrem/bean_annotation/App.java
+++ Spring-01-Core-IoC/src/main/java/com/akrem/bean_annotation/App.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-       ApplicationContext container = new AnnotationConfigApplicationContext();
+       ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
+        Object str = container.getBean("str");
+        System.out.println(str);
 
 
-
-
+        FullTimeMentor bean = container.getBean(FullTimeMentor.class);
+        bean.createAccount();
 
 
     }
---
+++ Spring-01-Core-IoC/src/main/java/com/akrem/bean_annotation/ConfigAny.java
@@ -0,0 +1,12 @@
+package com.akrem.bean_annotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigAny {
+    @Bean
+    String str(){
+        return "Developer";
+    }
+}
