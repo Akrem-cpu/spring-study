26eb1782436ca03c13596c4202fbae5ca6cdefe1
akrem-cpu <akremabdu71@gmail.com>
2022-12-25 20:53:31 -0600

    BeanScope

--- Spring-05-core-BeanScope/src/main/java/com/akrem/serivce/CommentService.java
+++ Spring-05-core-BeanScope/src/main/java/com/akrem/serivce/CommentService.java
@@ -5,12 +5,14 @@ import com.akrem.proxy.CommentNotificationProxy;
 import com.akrem.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Scope("prototype")
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
@@ -18,6 +20,7 @@ public class CommentService {
     public CommentService(CommentRepository commentRepository,@Qualifier("email") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("hello");
     }
 
     public void publishComment(Comment comment){
