3827b2c4b9b25a94c48b9be687232788bd7193bc
akrem-cpu <akremabdu71@gmail.com>
2023-01-16 13:45:41 -0600

    MVC controller

--- Spring-07-Spring-MVC-controller/src/main/java/com/akrem/controller/HomeController.java
+++ Spring-07-Spring-MVC-controller/src/main/java/com/akrem/controller/HomeController.java
@@ -3,11 +3,11 @@ package com.akrem.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // Annotate the class with the @controller Stereotype annotation
 public class HomeController {
-    @RequestMapping("/home")
+    @RequestMapping("/home")// user the @requestMapping annotation to associate with and HTTP request path
     public String home(){
-        return "home.html";
+        return "home.html"; // Return the HTML document name that contains the details we want the browser to display
     }
     @RequestMapping("/welcome")
     public String home2(){
