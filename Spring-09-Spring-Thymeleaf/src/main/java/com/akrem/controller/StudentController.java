8e02c36f2406f02b24025ce03f8812fb3c9dc0df
akrem-cpu <akremabdu71@gmail.com>
2023-01-26 05:36:57 -0600

    Spring thymeleaf

--- Spring-09-Spring-Thymeleaf/src/main/java/com/akrem/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/akrem/controller/StudentController.java
@@ -4,12 +4,13 @@ import com.akrem.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
       @RequestMapping("register")
-      public String register(Model model){
+      public String register(Model model ){
           model.addAttribute("students", DataGenerator.createStudent());
 
 
@@ -17,9 +18,10 @@ public class StudentController {
           return "/student/register";
       }
     @RequestMapping("info")
-    public String info(Model model){
+    public String info(Model model, @RequestParam(required = false) Integer id,@RequestParam(required = false) String name){
         model.addAttribute("students", DataGenerator.createStudent());
-
+        System.out.println(id);
+        System.out.println(name);
 
 
         return "/student/info";
