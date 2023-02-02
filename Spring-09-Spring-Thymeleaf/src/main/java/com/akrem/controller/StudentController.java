7fa6ef163f0d0c1c6484fd4630a63883ea27f3a3
akrem-cpu <akremabdu71@gmail.com>
2023-02-01 21:59:18 -0600

    Thymeleaf Form and condition

--- Spring-09-Spring-Thymeleaf/src/main/java/com/akrem/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/akrem/controller/StudentController.java
@@ -3,21 +3,24 @@ package com.akrem.controller;
 import com.akrem.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-      @RequestMapping("register")
+     // @RequestMapping(value = "register",method = RequestMethod.GET)
+      @GetMapping("/register")
       public String register(Model model ){
           model.addAttribute("students", DataGenerator.createStudent());
 
 
 
-          return "/student/register";
+          return "/student/register-student";
       }
-    @RequestMapping("info")
+    @RequestMapping("/info")
     public String info(Model model, @RequestParam(required = false) Integer id,@RequestParam(required = false) String name){
         model.addAttribute("students", DataGenerator.createStudent());
         System.out.println(id);
